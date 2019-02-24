e8a6c82c1eab5c245f5768774408f40f0a72b82b
anastazjagaluza <38051431+anastazjagaluza@users.noreply.github.com>
2019-02-24 14:40:56 +0100

    more responsveness in css and js

--- maping.js
+++ maping.js
@@ -11,7 +11,7 @@ let outputImgData;
 let greyvalue;
 let newData;
 let mapImg = new Image();
-let MaxMovement = 10;
+let MaxMovement;
 //responsiveness
 const x = window.matchMedia("(max-width: 700px)");
 let touchstartX = 0;
@@ -19,15 +19,27 @@ let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
 
+if (x.matches) {
+  originalImg.src = "smallkid.png";
+  mapImg.src = "smallmap.png";
+  canvasOriginal.canvas.width = 200;
+  canvasOriginal.canvas.height = 184;
+} else {
+  originalImg.src = "abort.jpg";
+  mapImg.src = "kidmap.jpg";
+  canvasOriginal.width = 480;
+  canvasOriginal.height = 440;
+}
 mapImg.addEventListener("load", function() {
   canvasOriginal.drawImage(mapImg, 0, 0);
   getMapImgData();
   canvasOriginal.drawImage(originalImg, 0, 0);
   getOriginalImgData();
 
-  outputImgData = canvasOriginal.createImageData(480, 440);
   //drawCat();
   if (x.matches) {
+    outputImgData = canvasOriginal.createImageData(200, 184);
+    MaxMovement = 3;
     let clientX;
     let clientY;
     let deltaX, deltaY;
@@ -48,22 +60,22 @@ mapImg.addEventListener("load", function() {
         let mouseX = deltaX;
         let mouseY = deltaY;
 
-        let ratioX = (mouseX / 480) * 2 - 1;
-        let ratioY = ((mouseY - 0) * (1 - -1)) / (440 - 0) + -1;
+        let ratioX = (mouseX / 200) * 2 - 1;
+        let ratioY = ((mouseY - 0) * (1 - -1)) / (184 - 0) + -1;
         let displacementX = ratioX * MaxMovement;
         let displacementY = ratioY * 1;
 
         console.log(ratioX, ratioY);
 
-        for (let y = 0; y < 440; y++) {
-          for (let x = 0; x < 480; x++) {
-            let n = (x + y * 480) * 4;
+        for (let y = 0; y < 184; y++) {
+          for (let x = 0; x < 200; x++) {
+            let n = (x + y * 200) * 4;
 
             greyvalue = mapImgData.data[n] / 255;
             let offsetX = Math.round(x + displacementX * greyvalue);
             let offsetY = Math.round(y + displacementY * greyvalue);
 
-            let i = (offsetX + offsetY * 480) * 4;
+            let i = (offsetX + offsetY * 200) * 4;
             outputImgData.data[n + 0] = originalImgData.data[i];
             outputImgData.data[n + 1] = originalImgData.data[i + 1];
             outputImgData.data[n + 2] = originalImgData.data[i + 2];
@@ -75,14 +87,12 @@ mapImg.addEventListener("load", function() {
       false
     );
   } else {
+    MaxMovement = 10;
     originalBox.addEventListener("mousemove", registerCoords);
+    outputImgData = canvasOriginal.createImageData(480, 440);
   }
 });
 
-originalImg.src = "abort.jpg";
-mapImg.src = "kidmap.jpg";
-
-function touchCoordinates(event) {}
 function registerCoords(event) {
   let mouseX = event.offsetX;
   let mouseY = event.offsetY;
